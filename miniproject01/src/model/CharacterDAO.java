f79cb73360b40ce0b04670a6409698aa0e4a4440
DongHyeok Kim <briankim0910@gmail.com>
2024-01-26 17:32:38 +0900

    기능 넣을

---
+++ miniproject01/src/model/CharacterDAO.java
@@ -0,0 +1,23 @@
+package model;
+
+public class CharacterDAO {
+	
+
+	//떡잎 마을 배달
+	
+	//유치원 배달
+	
+	//잠잘때
+	public void sleep() {
+		
+	}
+	//초코비 먹을때
+	
+	//삥뜯길때	
+
+	
+	//레벨업
+	
+	
+	
+}
---
+++ miniproject01/src/model/ChracterDTO.java
@@ -0,0 +1,10 @@
+package model;
+
+public class ChracterDTO {
+	private int Level;	//레벨 
+	private int Exp;	//경험치 
+	private int Hp;		//체력 
+	private int Yg;		//요구르트개수
+	
+	
+}
