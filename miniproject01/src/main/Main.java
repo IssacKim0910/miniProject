ca07f241e5647c8712f9fef9b241e4a9497fcee3
DongHyeok Kim <briankim0910@gmail.com>
2024-01-26 15:36:34 +0900

    Revert "동혁 업데이트"
    This reverts commit 1bb167b6da5eda705b81bcdd3628903093261642.

--- miniproject01/src/main/Main.java
+++ miniproject01/src/main/Main.java
@@ -74,8 +74,6 @@ public class Main {
 				DTO info = controller.login(id, pw);
 				if (info != null) {
 					System.out.println(info.getNick() + "님 환영합니다.");
-				} else {
-					System.out.println("존재하지 않는 ID입니다");
 				}
 				System.out.println();
 
