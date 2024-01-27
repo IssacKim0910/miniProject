3b78fb0af82203d16cccd848d3dd6f36c3825230
auto2 <auto2@DESKTOP-AK6R31S>
2024-01-27 22:01:11 +0900

    종호 야쿠르트 레벨별 종류 선언 및 짱구 레벨업 하면 야쿠르트도 레벨업하게 선언

--- miniproject01/src/model/CharacterDAO.java
+++ miniproject01/src/model/CharacterDAO.java
@@ -1,21 +1,21 @@
 package model;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 public class CharacterDAO {
 	Connection conn = null;
 	PreparedStatement psmt = null;
 	ResultSet rs = null;
 
+	// 떡잎 마을 배달
 
-	//떡잎 마을 배달
-	
-	//유치원 배달
-	
-	
-	//잠잘때 미구현
+	// 유치원 배달
+
+	// 잠잘때 미구현
 	public void sleep(CharacterDTO character) {
 		if (character.getLevel() == 1) {
 			character.setHp(10);
@@ -41,8 +41,8 @@ public class CharacterDAO {
 //		}finally {
 //		close();
 	}
-	
-	//초코비 먹을때 
+
+	// 초코비 먹을때
 	public void eat(CharacterDTO character) {
 		character.setHp(character.getHp() + 3);
 
@@ -56,7 +56,7 @@ public class CharacterDAO {
 			psmt = conn.prepareStatement(sql);
 			psmt.setInt(1, a);
 			psmt.setString(2, dto.getNick());
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -64,61 +64,65 @@ public class CharacterDAO {
 		}
 
 	}
-	
 
-	// 판매
-	public int sellItem(Yg Yg) {
-        int Exp = Yg.getExp();
+	// 판매-진행중
+	public void sellYg();
 
-        if (Exp > 0) {
-            System.out.println("아이템을 판매하여 경험치 " + Exp + "를 획득하였습니다.");
-        } else if (Exp < 0) {
-            System.out.println("마이너스 경험치를 얻었습니다. 경험치가 " + Math.abs(Exp) + "만큼 감소하였습니다.");
-        } else {
-            System.out.println("아이템을 판매하여 경험치를 얻지 못했습니다.");
-        }
+	{
 
-        return Exp;
-    }
-	
+		if (Exp > 0) {
+			System.out.println("아이템을 판매하여 경험치 " + Exp + "를 획득하였습니다.");
+		} else if (Exp < 0) {
+			System.out.println("마이너스 경험치를 얻었습니다. 경험치가 " + Math.abs(Exp) + "만큼 감소하였습니다.");
+
+		}
+	}
+
 	// 경험치 누적
 	private CharacterDTO character;
 
-    public CharacterDAO(CharacterDTO character) {
-        this.character = character;
-    }
+	public CharacterDAO(CharacterDTO character) {
+		this.character = character;
+	}
 
-    public CharacterDAO() {
+	public CharacterDAO() {
 
 	}
+
 	public void increaseExp(int exp) {
-        character.setExp(character.getExp() + exp);
-        if (character.getExp() >= 20) {
-            levelUp();
-        }
-    }
+		character.setExp(character.getExp() + exp);
+		if (character.getExp() >= 20) {
+			levelUp();
+		}
+	}
 
-    public void levelUp() {
-        if (character.getLevel() < 5) {
-            character.setLevel(character.getLevel() + 1);
-            character.setExp(character.getExp() - 20);
-        }}
-    
-	//레벨업
+	public void levelUp() {
+		if (character.getLevel() < 5) {
+			character.setLevel(character.getLevel() + 1);
+			character.setExp(character.getExp() - 20);
+		}
+	}
+
+	// 레벨업
 	private void checkLevelUp(CharacterDTO character) {
-        if (character.getExp() >= 100) {
-            int currentLevel = character.getLevel();
-            int newLevel = currentLevel + 1;
+		CharacterDTO cDTO = new CharacterDTO("null", 0, 0, 0);
+		DTO dto = new DTO("id", "pw", "nick");
+		String sql = "UPDATE JJANG SET LV = LV + ? WHERE NICK = ?";
 
-            if (newLevel <= 5) {
-                character.setLevel(newLevel);
-                character.setExp(0);
-                System.out.println("레벨이 증가했습니다! 현재 레벨: " + newLevel);
-            } else {
-                System.out.println("최대 레벨에 도달했습니다!");
-            }
-        }
-    }
+		if (character.getExp() >= 100) {
+			int currentLevel = character.getLevel();
+			int newLevel = currentLevel + 1;
+
+			if (newLevel <= 5) {
+				character.setLevel(newLevel);
+				character.setExp(0);
+				System.out.println("레벨이 증가했습니다! 현재 레벨: " + newLevel);
+			} else {
+				System.out.println("최대 레벨에 도달했습니다!");
+			}
+		}
+	}
+
 	public DTO login(String id, String pw) {
 
 		DTO info = null;
@@ -150,6 +154,7 @@ public class CharacterDAO {
 		return info;
 
 	}
+
 	private void connection() {
 
 		try {
