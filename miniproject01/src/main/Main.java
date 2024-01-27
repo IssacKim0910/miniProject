61a871afbee6756d4970f1ad212f4688b2e67bef
j2000 <j2000@59.3.127.104>
2024-01-27 15:23:24 +0900

    승혁-메인수정

--- miniproject01/src/main/Main.java
+++ miniproject01/src/main/Main.java
@@ -35,9 +35,9 @@ public class Main {
 		DTO dto = null;
 
 		while (true) {
-			System.out.print("[1]회원등록  [2]로그인  [3]랭킹  [4]종료 >> ");
+			System.out.print("[1]회원가입  [2]로그인  [3]랭킹  [4]게임종료 >> ");
 			int menu = sc.nextInt();
-			if (menu == 1) {
+			if (menu == 1) { // 회원가입
 				System.out.println("회원 등록");
 				System.out.print("아이디 입력 : ");
 				String id = sc.next();
@@ -48,51 +48,107 @@ public class Main {
 				dto = new DTO(id, pw, nick);
 				int cnt = controller.join(dto);
 
-			} else if (menu == 2) {
-
+			} else if (menu == 2) { // 로그인
 				System.out.print("아이디 입력 : ");
 				String id = sc.next();
 				System.out.print("비밀번호 입력 : ");
 				String pw = sc.next();
 
 				DTO info = controller.login(id, pw);
-
+				int num = 0;
 				if (info == null) {
 					System.out.println("로그인 실패");
 				} else if (info != null) {
 					String name = info.getNick();
-					for(int i = 0; i < name.length(); i++) {
+					for (int i = 0; i < name.length(); i++) {
 						System.out.print(name.charAt(i));
 						try {
 							Thread.sleep(50);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
-					
+
 					}
 					System.out.println("님 환영합니다.");
 
-					while (true) {
-						System.out.print("난이도선택 >> [1]떡잎마을 [2]떡잎 유치원 [3]뒤로가기>> ");
-						int num = sc.nextInt();
-						if (num == 1) {
+					int life = r.random4(0);
+					String ya = "하루최대 배달 가능횟수 : " + life + "회";
+					for (int i = 0; i < ya.length(); i++) {
+						System.out.print(ya.charAt(i));
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					System.out.println();
+
+					while (life > 0) { // 게임진행
+						System.out.print("경로선택 >> [1]떡잎마을 [2]떡잎 유치원 [3]캐릭터정보 [4]로비로 돌아가기>> ");
+						num = sc.nextInt();
+
+						if (num == 1) { // 떡잎마을
+							life--;
 							System.out.println("떡잎마을 배달시작");
 							System.out.println(r.random3(0));
-						} else if (num == 2) {
+							System.out.println("남은 배달횟수 : " + life);
+
+						} else if (num == 2) { // 떡잎유치원
+							life--;
 							System.out.println("떡잎 유치원 배달시작");
 							System.out.println(r.random2(0));
+							System.out.println("남은 배달횟수 : " + life);
 
-						} else if (num == 3) {
+						} else if (num == 3) { // 캐릭터정보
+							System.out.println("레벨 : ");
+							System.out.println("HP : ");
+							System.out.println("초코비 : ");
+
+						} else if (num == 4) { // 뒤로가기
+							String end = "로비로 돌아가는중...";
+							for (int i = 0; i < end.length(); i++) {
+								System.out.print(end.charAt(i));
+
+								try {
+									Thread.sleep(50);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
+							}
+							System.out.println();
 							break;
-						} else {
+
+						}
+
+						else {
 							System.out.println("잘못 입력하셨습니다.");
 						}
+
+					}
+				}
+				if (num != 4) {
+					String end = "남은 배달 횟수를 모두 소진하여 종료합니다.";
+					for (int i = 0; i < end.length(); i++) {
+						System.out.print(end.charAt(i));
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					System.out.println();
+				}
+			} else if (menu == 3) { // 랭킹
+				for (int i = 0; i < 10; i++) {
+					System.out.println((i + 1) + "위 : " + "\t 레벨 : ");
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
 					}
 				}
 
-			} else if (menu == 3) {
-
-			} else if (menu == 4) {
+			} else if (menu == 4) { // 게임종료
 				System.out.println(
 						"\r\n" + "\r\n" + "                                ,~:!****!:~,.                         \r\n"
 								+ "                             .!#@@@@@@@@@@@@@=,                       \r\n"
